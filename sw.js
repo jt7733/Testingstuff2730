bf5657d14b0a95ab10a39fa176d474ec20757e8b
jt7733 <j.thomason321@gmail.com>
2026-07-12 17:39:13 +1000

    Cache secure app loader and offline parts

--- sw.js
+++ sw.js
@@ -1,58 +1,24 @@
-const CACHE_NAME = 'julie-voice-studio-v5-pwa-1';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './css/style-1.css',
-  './css/style-2.css',
-  './js/script-1.js',
-  './js/script-2.js',
-  './js/script-3.js',
-  './js/script-4.js',
-  './js/script-5.js',
-  './icons/icon-192.svg',
-  './icons/icon-512.svg',
-  './icons/maskable-512.svg'
+const CACHE_NAME='julie-voice-loader-v1';
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg",
+  "./icons/maskable-512.svg",
+  "./chunks/app-01.b64",
+  "./chunks/app-02.b64",
+  "./chunks/app-03.b64",
+  "./chunks/app-04.b64",
+  "./chunks/app-05.b64",
+  "./chunks/app-06.b64",
+  "./chunks/app-07.b64",
+  "./chunks/app-08.b64",
+  "./chunks/app-09.b64",
+  "./chunks/app-10.b64",
+  "./chunks/app-11.b64"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  if (request.method !== 'GET') return;
-  const url = new URL(request.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request).then(cached => cached || fetch(request).then(response => {
-      if (response.ok) {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-      }
-      return response;
-    }))
-  );
-});
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const u=new URL(event.request.url);if(u.origin!==location.origin)return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));}return response;})));});
