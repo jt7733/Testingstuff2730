a5292aedaf8741b938f17d6d000a82fd0b011310
jt7733 <j.thomason321@gmail.com>
2026-08-03 12:47:44 +1000

    Refresh Julie Voice Studio mobile cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='julie-voice-studio-2-v3';
+const CACHE='julie-voice-studio-2-v4';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./audio.js','./app-core.js','./app-tools.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg','./icons/maskable-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
